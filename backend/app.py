b22a4b9a1bb2827026dd8f57ed34cd0a2d18c677
José <jose.emmanuel.rojo@hotmail.com>
2024-10-11 10:12:32 -0600

    Mejoras de las mamadas que hizo Portela

--- backend/app.py
+++ backend/app.py
@@ -9,27 +9,39 @@ app.secret_key = 'your_secret_key'
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/', methods=['GET','POST'])
-def vectorizeAPI ():
+@app.route('/', methods=['GET', 'POST'])
+def vectorizeAPI():
     if request.method == 'POST':
-        if 'file-input-corpus' not in request.files:
-            flash("No se seleccionó ningún archivo", "error")
-            return redirect(url_for('vectorizeAPI'))
-        
-        file = request.files['file-input-corpus']
+        try:
+            if 'file-input-corpus' not in request.files:
+                flash("No se seleccionó ningún archivo", "error")
+                return redirect(url_for('vectorizeAPI'))
 
-        if file.filename == '':
-            flash("No se seleccionó ningún archivo", "error")
-            return redirect(url_for('vectorizeAPI'))
-        
-        if file and file.filename.endswith('.csv'):
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(filepath)
-            df = pd.read_csv(filepath, sep='\t')
-            main(df)
-            os.remove(filepath)
-            flash('Vectorización completada exitosamente.')
-            return redirect(url_for('vectorizeAPI'))
+            file = request.files['file-input-corpus']
+
+            if file.filename == '':
+                flash("No se seleccionó ningún archivo", "error")
+                return redirect(url_for('vectorizeAPI'))
+
+            if file and file.filename.endswith('.csv'):
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                file.save(filepath)
+
+                df = pd.read_csv(filepath, sep='\t')
+
+                main(df)
+
+                os.remove(filepath)
+
+                flash('Vectorización completada exitosamente.', 'message')
+            else:
+                flash('El archivo debe ser un CSV.', 'error')
+
+        except Exception as e:
+            flash(f'Ocurrió un error durante la vectorización: {str(e)}', 'error')
+
+        return redirect(url_for('vectorizeAPI'))
+
     return render_template('index.html')
 
 if __name__ == '__main__':
